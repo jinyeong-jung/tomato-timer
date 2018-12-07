d4cb436ed82050477873543789c2a7e21ec93f5a
jinyeong-jung <jinyeong32@gmail.com>
2018-12-07 15:09:30 +0900

    first commit

---
+++ components/Timer/presenter.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Platform, StatusBar } from "react-native";
+import Button from "../Button";
+
+function formatTime(time) {
+  let minutes = Math.floor(time / 60);
+  time -= minutes * 60;
+  let seconds = parseInt(time % 60, 10);
+  return `${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+}
+
+class Timer extends Component {
+  componentWillReceiveProps(nextProps) {
+    const currentProps = this.props;
+    if (!currentProps.isPlaying && nextProps.isPlaying) {
+      const timerInterval = setInterval(() => {
+        currentProps.addSecond();
+      }, 1000);
+      this.setState({
+        timerInterval
+      });
+    } else if (currentProps.isPlaying && !nextProps.isPlaying) {
+      clearInterval(this.state.timerInterval);
+    }
+  }
+
+  render() {
+    const {
+      isPlaying,
+      elapsedTime,
+      timeDuration,
+      startTimer,
+      restartTimer,
+      addSecond
+    } = this.props;
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden />
+        <View style={styles.upper}>
+          <Text style={styles.time}>
+            {formatTime(timeDuration - elapsedTime)}
+          </Text>
+        </View>
+        <View style={styles.lower}>
+          {!isPlaying && <Button iconName="play-circle" onPress={startTimer} />}
+          {isPlaying && (
+            <Button iconName="stop-circle" onPress={restartTimer} />
+          )}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#CE0B24"
+  },
+  upper: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  lower: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  time: {
+    color: "white",
+    fontSize: 120,
+    fontWeight: "100",
+    fontFamily: Platform.OS === "android" ? "sans-serif-light" : undefined
+  }
+});
+
+export default Timer;
